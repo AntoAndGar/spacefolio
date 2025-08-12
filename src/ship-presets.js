f0a39645706d9c903d501f454674bbb70d5b4e8e
antoandgar <antonioandgargiulo@gmail.com>
2025-08-12 19:45:41 +0200

    added preset file

---
+++ src/ship-presets.js
@@ -0,0 +1,9 @@
+export const SHIP_PRESETS = {
+  // keys = file name without extension, lowercase
+  "x-wing":           { yaw: -Math.PI,   scale: 0.07, offset: [0, 0.00, 0], shipRadius: 2.5, name: "X-Wing" },
+  "spaceship_rescue": { yaw: -Math.PI/2, scale: 2.55, offset: [0, 0.00, 0], shipRadius: 1.5, name: "Spaceship Rescue" },
+  "falcon":           { yaw:  Math.PI,   scale: 0.04, offset: [0, 0.00, 0], shipRadius: 1.8, name: "Millennium Falcon" },
+  "ufo_mk2":          { yaw: -Math.PI/2, scale: 1.20, offset: [0, 0.00, 0], shipRadius: 1.2, name: "UFO Mk2" },
+  "interceptor":      { yaw:  0,         scale: 0.65, shipRadius: 1.0, name: "Interceptor" },
+  "default":          { yaw: -Math.PI/2, scale: 1.55, offset: [0, 0.00, 0], shipRadius: 1.0, name: "Default Ship" }
+};
